dc480813ef19287b591d487f912c636010a5821e
Heena <heenashaikh1811@gmail.com>
2026-01-24 16:52:53 +0530

    java example

--- src/example1/Example1.java
+++ src/example1/Example1.java
@@ -5,12 +5,10 @@ public class Example1 {
 	public static void main(String[] args) {
 		int a;
 		a = 20;
-		long x =2000;
-		char gender ='f';
-		float percentage= 33.2f;
+		long x = 2000;
+		char gender = 'f';
+		float percentage = 33.23f;
 		boolean isPresent = false;
-		
-
+		System.out.println(a);
 	}
-
 }
