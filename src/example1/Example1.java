50fad66a6b814769c7cf5a3861575110fe00414b
Heena <heenashaikh1811@gmail.com>
2026-01-24 16:04:04 +0530

    Understanding varaibles

---
+++ src/example1/Example1.java
@@ -0,0 +1,16 @@
+package example1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a;
+		a = 20;
+		long x =2000;
+		char gender ='f';
+		float percentage= 33.2f;
+		boolean isPresent = false;
+		
+
+	}
+
+}
