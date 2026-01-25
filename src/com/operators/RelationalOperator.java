d4c52fb3ea593a5015674e3be39d06707ebdbcc5
Heena <heenashaikh1811@gmail.com>
2026-01-25 15:37:40 +0530

    RelationalOperators

---
+++ src/com/operators/RelationalOperator.java
@@ -0,0 +1,19 @@
+package com.operators;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+//		Relational Operators!!! Primitive Datatype and Numbers
+//		>, <, >=, <=, ==, != ---> conditional statements if, if else, if else if
+		int number1=5;
+		int number2=10;	
+		System.out.println(number1>number2);//false
+		System.out.println(number1<number2);//true
+		System.out.println(number1==number2);//== isequalto ---> false
+//		= ---> assignement to value variable vs == isequalto value
+		System.out.println(number1!=number2);//true
+		System.out.println(number1>=number2);//false
+		System.out.println(number1<=number2);//true
+	}
+
+}
