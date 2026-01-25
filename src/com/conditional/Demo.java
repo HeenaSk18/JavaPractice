eefae99ad2731aeeb8cfdf39582fde49cbb91124
Heena <heenashaikh1811@gmail.com>
2026-01-25 21:39:14 +0530

    basic understanding of ifelsestatement

---
+++ src/com/conditional/Demo.java
@@ -0,0 +1,24 @@
+package com.conditional;
+
+public class Demo {
+
+	public static void main(String[] args) {
+//		Conditional Staments: 
+//		If Statement ---> ?: = if else==ternary operator
+
+//		stack memory ---> LIFO --->Data Structure ---> Linear DS ---> excuation of Methods
+
+		System.out.println("Hii");
+		int c; // declearation ---> memory alocation
+		c = 10 - 5;
+//		boolean ---> true / false return ---> expression/ methoda result boolean only
+		if ((c > 10) && (c==5)){ //if codeblock
+//			dead code block ---> not excuation this condition
+			System.out.println("C is greater than 10");
+//			out ---> reference variable ---> Bank - b --> reference variable
+			System.out.println("Hello");
+		}
+
+	}
+
+}
---
+++ src/com/conditional/MaxOfTwoNumber.java
@@ -0,0 +1,18 @@
+package com.conditional;
+
+public class MaxOfTwoNumber {
+
+	public static void main(String[] args) {
+		int number1 = 24;
+		int number2 = -44;
+//		MAX?
+		if(number1>number2) {
+			System.out.println("Max is "+number1);
+		}
+		else {
+			System.out.println("Max is "+ number2);
+		}
+
+	}
+
+}
