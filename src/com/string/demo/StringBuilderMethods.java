139dd30508723129046244072e8c9973e99e7204
Heena <heenashaikh1811@gmail.com>
2026-01-29 16:10:46 +0530

    StringBuilderMethods

--- src/com/string/demo/StringBuilderMethods.java
+++ src/com/string/demo/StringBuilderMethods.java
@@ -3,7 +3,7 @@ package com.string.demo;
 public class StringBuilderMethods {
 
 	public static void main(String[] args) {
-//		mutual --- performance -- fast/high -- non sychronized --- not thread safe
+//		mutual --- performance good -- fast/high -- non sychronized --- not thread safe
 //		signle threaded application --- modification -- manipulate string --- loop
 		StringBuilder sb = new StringBuilder("hello");
 //		heap memory 
@@ -11,13 +11,13 @@ public class StringBuilderMethods {
 //		sb.append(" World:); // Hello World
 		
 //		2) insert(index,char)
-//		sb.insert(5,',') //Hello , World
+//		sb.insert(5,',') //Hello, World
 		
 //		3) delete(start, endindex)
 //		sb.delte(5,6)// Hello World
 		
 //		4) reverse()
-//		sb.reverse() --- dlrow olleH
+//		sb.reverse() ---> dlrow olleH
 		
 //		5) capacity()
 //		16 -- 16+5=21
